57963bff9efa629f3262dc7383290058608a0a42
JIHO <sulfur0525@naver.com>
2023-10-20 22:26:51 +0900

    스크롤

--- varo2/public/index.js
+++ varo2/public/index.js
@@ -1,1 +1,10 @@
 console.log("js 연결") 
+
+/* 스크롤 */
+function move(num){
+var menuHeight = document.querySelector(".menu").offsetHeight;
+
+var location = document.querySelector("#menu"+num).offsetTop;
+
+window.scrollTo({top:location - menuHeight, behavior:'smooth'});
+}
